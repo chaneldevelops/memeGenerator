e3f799ac3cdbb6223fc33524a1101843c65889d0
chaneldevelops <chanel.developsdesigns@gmail.com>
2023-05-23 23:31:52 -0400

    Altered header
    - changed the header to just display a title

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from "./components/Header"
 import Meme from "./components/Meme"
+import './index.css'
 
 
 export default function App() {
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,14 @@ import React from "react"
 export default function Header() {
     return (
         <header className="header">
-            <img 
+
+            <h1>Meme Generator</h1>
+            {/* <img 
                 src="../images/troll-face.png" 
                 className="header--image"
-            />
-            <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">React Course - Project 3</h4>
+            /> */}
+            {/* <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4> */}
         </header>
     )
 }
